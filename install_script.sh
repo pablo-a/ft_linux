5cd13c9d9f0c7eff8c6e5fc2a7f456923383bea3
Pablo ABRIL <pabril@student.42.fr>
2018-07-14 17:16:38 +0200

    add 2 scripts to help init VM

---
+++ install_script.sh
@@ -0,0 +1,5 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+# replace dash shell with bash
+sudo rm /bin/sh && sudo ln -s /bin/bash /bin/sh
+sudo apt-get install -y zsh ssh gcc vim git make perl bison bzip2 gawk g++ makeinfo xz texinfo curl
