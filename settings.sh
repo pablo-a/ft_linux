b0b6a8875123024080e02c8c714ec1e26ba7f780
Pablo ABRIL <pabril@student.42.fr>
2018-07-14 18:00:02 +0200

    update settings script

--- settings.sh
+++ settings.sh
@@ -1,4 +1,12 @@
 echo "export LFS=/mnt/lft" >> ~/.zshrc
+#create base dir.
 sudo mkdir -pv $LFS
+
 #mount root partition.
-mount -v -t ext4 /dev/sda4
+sudo mount -v -t ext4 /dev/sda4 $LFS
+sudo mount -v -t ext4 /dev/sda3 $LFS/boot
+
+# make source directory
+sudo mkdir $LFS/sources
+sudo chmod -v a+wt $LFS/sources
+
