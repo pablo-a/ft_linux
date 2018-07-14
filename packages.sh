55ed2e721552d463ced7aefa55547d55327b1bcd
Pablo ABRIL <pabril@student.42.fr>
2018-07-14 18:44:28 +0200

    update

--- packages.sh
+++ packages.sh
@@ -3,3 +3,8 @@ wget http://download.savannah.gnu.org/releases/acl/acl-2.2.52.src.tar.gz
 
 # DL Attr package.
 wget http://download.savannah.gnu.org/releases/attr/attr-2.4.47.src.tar.gz
+
+# DL Autoconf package.
+wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.xz
+
+
