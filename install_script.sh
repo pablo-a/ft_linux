3de872281b866c0f935e1f4b30eac6f647ec823d
Pablo ABRIL <pabril@student.42.fr>
2018-07-14 17:28:12 +0200

    add oh my zsh + sublivim install

--- install_script.sh
+++ install_script.sh
@@ -3,3 +3,7 @@ sudo apt-get upgrade -y
 # replace dash shell with bash
 sudo rm /bin/sh && sudo ln -s /bin/bash /bin/sh
 sudo apt-get install -y zsh ssh gcc vim git make perl bison bzip2 gawk g++ texinfo curl
+# install oh my zsh + set zsh as default shell
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+# install sublivim
+curl https://raw.githubusercontent.com/reversTeam/Sublivim/master/installer.sh | sh
