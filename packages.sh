2dc39ee28e0fcebb0bc61ffe33d30475eb7c5505
Pablo ABRIL <pabril@student.42.fr>
2018-07-14 18:09:19 +0200

    add packages dl script

---
+++ packages.sh
@@ -0,0 +1,5 @@
+# DL ACL package.
+wget http://download.savannah.gnu.org/releases/acl/acl-2.2.52.src.tar.gz
+
+# DL Attr package.
+wget http://download.savannah.gnu.org/releases/attr/attr-2.4.47.src.tar.gz
