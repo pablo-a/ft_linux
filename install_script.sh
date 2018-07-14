5bf4068ab929dc2ee7e62387c1c75a82acef5a80
Pablo ABRIL <pabril@student.42.fr>
2018-07-14 17:26:00 +0200

    remove 2 packages

--- install_script.sh
+++ install_script.sh
@@ -2,4 +2,4 @@ sudo apt-get update
 sudo apt-get upgrade -y
 # replace dash shell with bash
 sudo rm /bin/sh && sudo ln -s /bin/bash /bin/sh
-sudo apt-get install -y zsh ssh gcc vim git make perl bison bzip2 gawk g++ makeinfo xz texinfo curl
+sudo apt-get install -y zsh ssh gcc vim git make perl bison bzip2 gawk g++ texinfo curl
