ca55db54ed9860a9c4bb4efa7d37bb32328252ee
Pablo Abril <pabril@student.42.fr>
2018-07-15 12:25:30 +0200

    add packages url list

---
+++ dl_extract_packages.sh
@@ -0,0 +1,26 @@
+# DL ACL package.
+wget http://download.savannah.gnu.org/releases/acl/acl-2.2.52.src.tar.gz
+tar -xzvf acl-2.2.52.src.tar.gz
+
+# DL Attr package.
+wget http://download.savannah.gnu.org/releases/attr/attr-2.4.47.src.tar.gz
+tar -xzvf attr-2.4.47.src.tar.gz
+
+
+# DL Autoconf package.
+wget http://ftp.gnu.org/gnu/autoconf/autoconf-2.69.tar.xz
+tar xf autoconf-2.69.tar.xz
+
+# Automake package.
+wget http://ftp.gnu.org/gnu/automake/automake-1.15.1.tar.xz
+tar xf automake-1.15.1.tar.xz
+
+# bash
+wget http://ftp.gnu.org/gnu/bash/bash-4.4.18.tar.gz
+tar xf bash-4.4.18.tar.gz
+
+# bc
+wget http://ftp.gnu.org/gnu/bc/bc-1.07.1.tar.gz
+tar xf bc-1.07.1.tar.gz
+
+#
