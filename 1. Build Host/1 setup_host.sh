1bf345ad4b7d8bea7807557ae259219d8aada24f
Pablo Abril <pabril@student.42.fr>
2019-03-27 10:31:06 +0100

    phase 1

--- 1. Build Host/1 setup_host.sh
+++ 1. Build Host/1 setup_host.sh
@@ -1,8 +1,7 @@
 sudo apt-get update
 sudo apt-get upgrade -y
-# replace dash shell with bash
-sudo rm /bin/sh && sudo ln -s /bin/bash /bin/sh
-sudo apt-get install -y zsh ssh gcc vim git make perl bison bzip2 gawk g++ texinfo curl
+
+sudo apt-get install -y zsh 
 # install oh my zsh + set zsh as default shell
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # install sublivim
