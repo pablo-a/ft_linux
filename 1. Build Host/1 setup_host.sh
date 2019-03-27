b7309767ad5482d249132880d2f04ced2374b5d1
Pablo Abril <pabril@student.42.fr>
2019-03-27 10:33:32 +0100

    replace apt by dnf

--- 1. Build Host/1 setup_host.sh
+++ 1. Build Host/1 setup_host.sh
@@ -1,7 +1,7 @@
-sudo apt-get update
-sudo apt-get upgrade -y
+sudo dnf update
+sudo dnf upgrade -y
 
-sudo apt-get install -y zsh 
+sudo dnf install -y zsh 
 # install oh my zsh + set zsh as default shell
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # install sublivim
